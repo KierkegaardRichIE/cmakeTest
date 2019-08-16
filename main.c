45eb3a7d0f4c1a49c477f7a29eed54012eba9887
KierkegaardRichIE <kierke7@outlook.com>
2019-08-16 18:15:26 +0800

    Add the function--strlen for return the length of a string.

--- main.c
+++ main.c
@@ -6,10 +6,17 @@ void swap(int *px,int *py){
     *px=*py;
     *py=temp;
 }
+int strlen(char *s){
+    int n;
+    for (n = 0; *s != '\0';s++)
+        n++;
+    return n;
+}
 int main(){
     int a=10;
     int b=7;
     swap(&a,&b);
-    printf("%d\t%d",a,b);
+    printf("%d\t%d\n",a,b);
+    printf("%d", strlen("hello, world"));
     return 0;
 }
